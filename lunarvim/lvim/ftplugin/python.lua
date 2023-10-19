3a736580fde3381eb7fd7850476927faed72de96
matqsarlab <m.balicki@qsarlab.com>
2023-10-19 15:04:58 +0200

    My Configuration

---
+++ lunarvim/lvim/ftplugin/python.lua
@@ -0,0 +1,43 @@
+-- TODO: debugpy installed by default
+-- Setup dap for python
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+pcall(function() require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python") end)
+
+-- Supported test frameworks are unittest, pytest and django. By default it
+-- tries to detect the runner by probing for pytest.ini and manage.py, if
+-- neither are present it defaults to unittest.
+pcall(function() require("dap-python").test_runner = "pytest" end)
+
+-- Mappings
+lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('dap-python').test_method()<cr>", "Test Method" }
+lvim.builtin.which_key.mappings["df"] = { "<cmd>lua require('dap-python').test_class()<cr>", "Test Class" }
+lvim.builtin.which_key.vmappings["d"] = {
+  name = "Debug",
+  s = { "<cmd>lua require('dap-python').debug_selection()<cr>", "Debug Selection" },
+}
+
+
+-- Additional Plugins
+lvim.plugins = {
+  -- You can switch between vritual environmnts.
+  "AckslD/swenv.nvim",
+  "mfussenegger/nvim-dap-python",
+  {
+    -- You can generate docstrings automatically.
+    "danymat/neogen",
+    config = function()
+      require("neogen").setup {
+        enabled = true,
+        languages = {
+          python = {
+            template = {
+              annotation_convention = "numpydoc",
+            },
+          },
+        },
+      }
+    end,
+  },
+}
+
